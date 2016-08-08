ed04da85ed1309c9f4b9a997f1306942a0f1bb69
Chase Vedder <chasevedder@gmail.com>
2016-08-08 02:38:58 -0700

    Added blog detail page, changed some styling

--- website/urls.py
+++ website/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from . import views
 
-namespace = 'website'
+app_name = 'website'
 
 urlpatterns = [
-    url(r'^', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<post_id>[0-9]+)', views.post_detail, name='post-detail'),
 ]
--- website/views.py
+++ website/views.py
@@ -5,3 +5,7 @@ from .models import BlogPost
 # Create your views here.
 def index(request):
     return render(request, 'website/index.html', {'blogs': BlogPost.objects.all()})
+
+def post_detail(request, post_id):
+    post = BlogPost.objects.get(pk=post_id)
+    return render(request, 'website/post_detail.html', {'post': post})
